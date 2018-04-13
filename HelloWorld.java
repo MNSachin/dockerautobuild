cba8f7946ac000333290765ee450ed558da5e6ed
MNSachin <mnsachinjamadagni@gmail.com>
2018-04-13 09:21:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-      System.out.println("Qick Change");
-      System.out.println("Anil bidari");
+      System.out.println("Hello, Sachin M N");
    }
 }
